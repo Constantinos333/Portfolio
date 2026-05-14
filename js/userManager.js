1ad8a8b86e53308d0af05830e512b9b350e3db6c
Constantinos <218368280+Constantinos333@users.noreply.github.com>
2026-05-14 10:56:22 +0300

    Completed CRUD by adding DELETE. Otimized UPDATE by excluding ID and Timestamp from updating

--- js/userManager.js
+++ js/userManager.js
@@ -3,12 +3,14 @@ const userManager = {
     // 1. LOAD: Initialize users from localStorage (if they exist), otherwise use empty array
     users: JSON.parse(localStorage.getItem('users')) || [],
 
+    //CREATE: Accepts the externally-created user object as is
     addUser(user) {
         this.users.push(user);
         // 2. SAVE: Convert the updated array to a string and save it to localStorage
         localStorage.setItem('users', JSON.stringify(this.users));
     },
 
+    //READ: Returns a new shallow copy of all users
     getAllUsers() {
         return [...this.users];
     },
@@ -25,15 +27,22 @@ const userManager = {
         return this.users.find((user) => user.id === id);
     },
 
+    //UPDATE: Reassigns the array and protects 'id' & 'createdAt'
     updateUser(id, updates) {
         this.users = this.users.map((user) => {
             if (user.id === id) {
-                return { ...user, ...updates };
+                return { ...user, ...updates, id: user.id, createdAt: user.createdAt };
             }
             return user;
-        }); //This needs work, as of now it may update UID and TimeStamp which are meant to be immutable
+        });
 
         localStorage.setItem('users', JSON.stringify(this.users));
+        //returns the updated user object
+    },
+
+    //DELETE: Reassigns the array to exclude the specific ID
+    deleteUser(id) {
+        this.users = this.users.filter(user => user.id !== id);
     }
 
 };
